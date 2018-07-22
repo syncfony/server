d7ab55eacef08a5f699a4b74cc5957c13b4f15d2
cypherix93 <bik@egifter.com>
2018-07-21 23:49:09 -0400

    Some work.

---
+++ Syncfony.Api/Models/MidiMessagecs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Syncfony.Api.Models
+{
+    public class MidiMessage
+    {
+        public MidiMessageState State { get; set; }
+        public int Note { get; set; }
+        public int Velocity { get; set; }
+    }
+
+    public enum MidiMessageState
+    {
+        Hit,
+        Release
+    }
+}
---
+++ Syncfony.Api/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfony.Api.Hubs;
+
+namespace Syncfony.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            //app.UseSignalR(routes =>
+            //{
+            //    routes.MapHub<NotesHub>("/notes");
+            //});
+        }
+    }
+}
